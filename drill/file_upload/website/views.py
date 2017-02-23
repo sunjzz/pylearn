0f2b54eeaa2f75e4de44c2336899a3abd635b510
sunjzz <sun_jzz@163.com>
2017-02-23 23:05:07 +0800

    django 学习

--- drill/file_upload/website/views.py
+++ drill/file_upload/website/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import os
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        file = request.FILES.get('file')
+        f = open(os.path.join('data', file.name), 'wb')
+        for chunk in file.chunks():
+            f.write(chunk)
+        f.close()
+    return render(request, 'index.html')
