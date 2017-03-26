b7c2d6e8ae86c446a690595bfac631950810e65b
sunjzz <sun_jzz@163.com>
2017-03-26 21:29:54 +0800

    update

--- drill/mooc/apps/users/views.py
+++ drill/mooc/apps/users/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+
+import models
+
+class CustomBackend(ModelBackend):
+    def authenticate(self, username=None, password=None, **kwargs):
+        try:
+            user = models.UserProfile.objects.get(Q(username=username)|Q(email=username), Q(password=password))
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
+
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        user_name = request.POST.get("username", "")
+        pass_word = request.POST.get("password", "")
+        user = authenticate(usernmae = user_name, password = pass_word)
+        if user is not None:
+            login(request, user)
+            render(request, "index.html")
+    if request.method =='GET':
+        return render(request, 'login.html', {})
--- drill/mooc/mooc/urls.py
+++ drill/mooc/mooc/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 import xadmin
+
+
+from users.views import user_login
 
 urlpatterns = [
     # Examples:
@@ -9,4 +13,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^xadmin/', include(xadmin.site.urls)),
+    url('^$', TemplateView.as_view(template_name = "index.html"), name= "index"),
+    url(r'^login/$', user_login, name= "login")
 ]
